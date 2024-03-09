24b0a85139e12a3f3ba5a463f7e8b4b7825d6a5c
DangShark <23020356@vnu.edu.vn>
2024-03-09 23:15:49 +0700

    StringCodeHW2

---
+++ HW/StringCode.java
@@ -0,0 +1,87 @@
+import java.util.HashSet;
+import java.util.Set;
+
+// CS108 HW1 -- String static methods
+
+public class StringCode {
+
+	/**
+	 * Given a string, returns the length of the largest run.
+	 * A a run is a series of adajcent chars that are the same.
+	 * @param str
+	 * @return max run length
+	 */
+	public static int maxRun(String str) {
+		int d1 = 1;
+		int max = 1;
+		if(str == ""){
+			return 0;
+		}
+		for(int i = 1; i < str.length(); ++i){
+			if(str.charAt(i) == str.charAt(i-1)){
+				d1++;
+			}
+			else{
+				max = Math.max(d1,max);
+				d1 = 1;
+			}
+		}
+		max = Math.max(max,d1);
+		return max;
+    }
+
+	
+	/**
+	 * Given a string, for each digit in the original string,
+	 * replaces the digit with that many occurrences of the character
+	 * following. So the string "a3tx2z" yields "attttxzzz".
+	 * @param str
+	 * @return blown up string
+	 */
+	public static String blowup(String str) {
+		String s = new String();
+		for(int i = 1; i < str.length(); i++){
+			if(str.charAt(i-1) >= '0' && str.charAt(i-1) <= '9'){
+				int m = Character.getNumericValue(str.charAt(i-1));
+				if(!Character.isDigit(str.charAt(i))){
+					s += (str.charAt(i));
+				}
+				for(int j = 1; j <= m; j++){
+					s += (str.charAt(i));
+				}
+			}
+			else{
+				if(i-1 == 0){
+					s += (str.charAt(i-1));
+				}
+				else if(!Character.isDigit(str.charAt(i-2)))
+					s += str.charAt(i-1);
+				 if(i == str.length()-1){
+					if(!Character.isDigit(str.charAt(i))){
+						s += (str.charAt(i));
+					}
+				}
+			}
+		}
+		return s;
+	}
+
+	/**
+	 * Given 2 strings, consider all the substrings within them
+	 * of length len. Returns true if there are any such substrings
+	 * which appear in both strings.
+	 * Compute this in linear time using a HashSet. Len will be 1 or more.
+	 */
+	public static boolean stringIntersect(String a, String b, int len) {
+		HashSet<String> st = new HashSet<>();
+		for (int i = 0; i <= a.length() - len; i++) {
+			st.add(a.substring(i, i + len));
+		}
+		for (int i = 0; i <= b.length() - len; i++) {
+			if (st.contains(b.substring(i, i + len))) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
